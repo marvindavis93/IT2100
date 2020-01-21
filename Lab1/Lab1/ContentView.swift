439218af2954a397e36fb8f4d613013f8ab75ae3
Marvin Davis <S01024543@acad.tri-c.edu>
2020-01-21 15:35:23 -0500

    Created Lab 1

---
+++ Lab1/Lab1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lab1
+//
+//  Created by Marv Davis on 1/21/20.
+//  Copyright © 2020 Marv Davis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World! from Marvin!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
