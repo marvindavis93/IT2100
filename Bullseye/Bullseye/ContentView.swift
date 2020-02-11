66e9e8e801621523a450464baa2f86b67c42a6b5
Marvin Davis <S01024543@acad.tri-c.edu>
2020-02-10 21:27:39 -0500

    Bullseye 11-20

--- Bullseye/Bullseye/ContentView.swift
+++ Bullseye/Bullseye/ContentView.swift
@@ -9,57 +9,92 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    
+
     @State var alertIsVisible: Bool = false
-    @State var whosThereIsVisible: Bool =  false
-    
-    
-    
+    @State  var sliderValue : Double =  50.0
 var body: some View {
-    VStack {
-        Text("Hello, World!")
-            .fontWeight(.bold)
-            .foregroundColor(Color.green)
-        Button(action: {
-            print ("button pressed!")
-            self.alertIsVisible = true
         
-        
-            }) {
-            Text("Hit me ")
-        }
-        
-        .alert(isPresented:$alertIsVisible) { () ->
-        Alert in
-        return Alert(title: Text("Hello there!"),
-            message: Text ("This is my first pop - up!"),
-            dismissButton: .default(Text("Awesome")))
-    }
-        Button(action: {
-            self.whosThereIsVisible = true
-               
-            
-        }){
-                Text("Knock Knock")
+   
+  //Target row
+    HStack {
+        VStack {
+        VStack {
+            HStack {
+                VStack {
+                    HStack {
+                        Text("Put the bullseye as close as you can to:")
+                        Text("100")
+                    }
+                }
+                         
             }
+    }
+            Spacer()
+      //Slider row
             
+            HStack{
+                Text("1")
+                
+                Slider(value: self.$sliderValue, in : 1...100)
+                Text("100")
+                
+            }
+        
+            Spacer()
+        //Button row
+        
+    Button(action: {
+        print("button pressed!")
+        self.alertIsVisible = true
+    }) {
+        Text("Hit me")
+    }
+    .alert(isPresented: $alertIsVisible)  { () ->
+        Alert in
+        return Alert(title: Text("Hello There!"),
+                     message: Text ("The slider's value is  \(self.sliderValue)."),
+        dismissButton: .default(Text("Awesome!")));
+        }
             
-            .alert(isPresented:$whosThereIsVisible) { () ->
-            Alert in
-            return Alert(title: Text("Who's There!"),
-                message: Text ("Cow!"),
-                dismissButton: .default(Text("Cow Who?,  Moooooove Over!")));
+            Spacer()
+        //Score row
+            HStack{
+                Text("Start over")
+                Spacer()
+                Text ("Score")
+                Text("999999")
+                Spacer()
+
+                Text("Round")
+                Text("999")
+                Spacer()
+                Button(action: {} ) {
+                    Text ("Info")
+                }
+                
+        
+            }
+            .padding(.bottom, 20)
         }
         
     }
+}
         
-    }
-    }
+        
+        
+        
+        
+        
+        
+        
+        
+       
+    
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView() .previewLayout( .fixed(width: 896, height: 414))
     }
 }
 
 
+}
