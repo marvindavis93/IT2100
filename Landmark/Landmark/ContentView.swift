bbc0179b0ea218ca13935897e1a5ea8a9447481d
Marvin Davis <S01024543@acad.tri-c.edu>
2020-05-07 12:38:59 -0400

    Lab 8

---
+++ Landmark/Landmark/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmark
+//
+//  Created by Marv Davis on 5/7/20.
+//  Copyright © 2020 Marv Davis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
